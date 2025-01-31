bf332ec264100480b8ea5a09406d88c7f15946cc
Solobored <josneiba10@gmail.com>
2025-01-31 12:08:51 -0300

    commit

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -9,7 +9,10 @@ document.addEventListener("DOMContentLoaded", () => {
         { templeName: "Thailand Temple", location: "Bangkok, Thailand", dedicated: "2019, October, 22", area: 48525, imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/bangkok-thailand-temple/bangkok-thailand-temple-40037-main.jpg" },
         { templeName: "Manti Utah", location: "Manti, Utah, United States", dedicated: "1888, May, 21", area: 74792, imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg" },
         { templeName: "Salt Lake City", location: "Salt Lake City, Utah, United States", dedicated: "1893, April, 24", area: 382207, imageUrl: "https://churchofjesuschristtemples.org/assets/img/temples/salt-lake-temple/salt-lake-temple-15669-main.jpg" },
-        {templeName: "Mexico City Mexico",location: "Mexico City, Mexico",dedicated: "1983, December, 2",area: 116642,imageUrl:"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"}
+        {templeName: "Mexico City Mexico",location: "Mexico City, Mexico",dedicated: "1983, December, 2",area: 116642,imageUrl:"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"},
+        { templeName: "Aba Nigeria",location: "Aba, Nigeria",dedicated: "2005, August, 7",area: 11500,imageUrl:"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"},
+        {templeName: "Payson Utah",location: "Payson, Utah, United States",dedicated: "2015, June, 7",area: 96630,imageUrl:"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"},
+        {templeName: "Yigo Guam",location: "Yigo, Guam",dedicated: "2020, May, 2",area: 6861,imageUrl:"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"}
     ];
     
     const container = document.getElementById("temple-container");
